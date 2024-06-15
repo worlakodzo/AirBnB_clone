16a408a7188c87ad66affd39190e7b57bcb0ca52
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-06-15 17:28:43 +0000

    Updated base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,3 +10,14 @@ class BaseModel:
 
     def __str__(self) -> str:
         return f"[{self.__class__}] (<{self.id}>) <{str(self.__dict__)}>"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        return {
+            "__class__": str(self.__class__),
+            "id": self.id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+        }
