3d0c080ac4782b73d191872eb86ca92f9e536fcc
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-06-15 21:10:47 +0000

    Updated basemodel

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
+import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+class TestBaseModelDict(unittest.TestCase):
+
+    def test_is_id_string(self):
+        b1 = BaseModel()
+        self.assertTrue(isinstance(b1.id, str))
+
+    def test_created_at_a_datetime(self):
+        b1 = BaseModel()
+        self.assertTrue(isinstance(b1.created_at, datetime))
+
+    def test_compare_two_obj_id(self):
+        b1 = BaseModel()
+        b2 = BaseModel()
+
+        self.assertFalse(b1.id == b2.i)
+
+
+if __name__ == "__main__":
+    unittest.main()
