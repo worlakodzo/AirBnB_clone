2cde60910a0d2123d2069577844a8149a5935987
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-06-15 13:18:44 +0000

    aaded console

--- console.py
+++ console.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-print("(hbnb)")
+
+"""
+This is an example module that demonstrates writing beautiful code
+which passes pycodestyle checks.
+"""
+
+
+def console_print():
+    """
+    print text to the stdout
+    """
+
+    print("(hbnb) help\n")
+
+
+if __name__ == "__main__":
+    console_print()
